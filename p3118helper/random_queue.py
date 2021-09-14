cb1457a1f717494fb029cce178c44397858f506b
Andrew <tankist.scratch.p@gmail.com>
2021-09-14 16:28:43 +0300

    Fixing RandomQueueBot class name; PyCharm environment

--- p3118helper/random_queue.py
+++ p3118helper/random_queue.py
@@ -1,5 +1,5 @@
 from ._base import BaseBot
 
-class RandoRandomQueueBot(BaseBot):
+class RandomQueueBot(BaseBot):
     def __init__(self, token, *, group_id, **kwargs):
         super().__init__(token, group_id=group_id)
