b35e67d307c1a968493cf3df79998910e3a960d5
Andrew <tankist.scratch.p@gmail.com>
2021-10-17 11:55:55 +0300

    Preparing prerelease (v0.2.0rc0)

--- readme.md
+++ readme.md
@@ -4,7 +4,7 @@ Telegram bot on `aiogram` for group P3118 in [ITMO](https://itmo.ru/)
 
 ## **Installing**
 
-`pip install https://github.com/LandgrafHomyak/P3118Helper-cpython/releases/download/v0.1.0/P3118Helper-0.1.0-py3-none-any.whl`
+`pip install https://github.com/LandgrafHomyak/P3118Helper-cpython/archive/refs/tags/v0.2.0rc0.tar.gz`
 
 ## **Using**
 
--- setup.py
+++ setup.py
@@ -7,19 +7,22 @@ ext_queue = Extension(
 
 setup(
     name="P3118Helper",
-    packages=["p3118helper"],
     version="0.2.0rc0",
     install_requires=["aiogram"],
+    packages=["p3118helper"],
+    package_data={"p3118helper": ["py.typed", "*.pyi"]},
     url="https://github.com/LandgrafHomyak/P3118Helper-cpython",
     author="Andrew Golovashevich",
     python_requires=">=3.8",
     classifiers = [
-        "Development Status :: 5 - Production/Stable",
+        "Development Status :: 4 - Beta",
         "Framework :: AsyncIO",
         "Natural Language :: Russian",
         "Operating System :: OS Independent",
+        "Programming Language :: C",
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: CPython",
         "Typing :: Typed"
     ],
     ext_package="p3118helper",
