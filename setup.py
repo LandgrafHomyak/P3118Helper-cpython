8e17e29d13484adc1d749d9d2c29d3c127c2afea
Andrew Golovashevich <tankist.scratch.p@gmail.com>
2021-11-25 14:06:51 +0300

    Updating setup script

--- setup.py
+++ setup.py
@@ -7,16 +7,19 @@ ext_queue = Extension(
 
 setup(
     name="P3118Helper",
-    version="0.2.0rc0",
+    version="0.3.0rc0",
     install_requires=["aiogram"],
     packages=["p3118helper"],
+    description="Telegram bot on aiogram for group P3118 in ITMO",
     package_data={"p3118helper": ["py.typed", "*.pyi"]},
     url="https://github.com/LandgrafHomyak/P3118Helper-cpython",
     author="Andrew Golovashevich",
-    python_requires=">=3.8",
+    download_url="https://github.com/LandgrafHomyak/P3118Helper-cpython/releases/tag/v0.3.0rc0",
+    python_requires=">=3.8, <3.12",
     classifiers = [
         "Development Status :: 4 - Beta",
         "Framework :: AsyncIO",
+        "License :: OSI Approved :: MIT License",
         "Natural Language :: Russian",
         "Operating System :: OS Independent",
         "Programming Language :: C",
@@ -28,5 +31,7 @@ setup(
     ext_package="p3118helper",
     ext_modules=[
         ext_queue
-    ]
+    ],
+    license="MIT",
+    license_files=["LICENSE"],
 )
